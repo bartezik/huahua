cfb4a647ca20c813491712451e42f1e1695f27a1
bartezik <szevco@gmail.com>
2018-10-24 01:22:53 +0200

    cos na poczatek

---
+++ app/src/main/java/com/example/bartez/gamehakeha/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.bartez.gamehakeha;
+
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // getActionBar().setIcon(R.drawable.logo);
+
+
+        setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setNavigationIcon(R.drawable.logo50);
+      //  actionbar.setDisplayHomeAsUpEnabled(true);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
